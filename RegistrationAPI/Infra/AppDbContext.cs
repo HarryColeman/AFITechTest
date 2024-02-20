cce32b121a46347c2f49b3b9fb2e388d780849cf
hcoleman.r@gmail.com <hcoleman.r@gmail.com>
2024-02-20 18:23:08 +0000

    Added Infra skeleton

---
+++ RegistrationAPI/Infra/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) { }
+
+    // Need to pull domain through App
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
---
+++ RegistrationAPI/Infra/DI.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class DI
+{
+    // Methods requires to wire this up in the WebProj
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Get From Web Proj Later
+        var connString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<AppDbContext>(opts => opts.UseSqlite(connString));
+
+        return services;
+    }
+
+    public static async Task InitialiseDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        try
+        {
+            await ctx.Database.MigrateAsync();
+        }
+        catch (Exception)
+        {
+            // Consider logging or some other handling?
+            throw;
+        }
+    }
+}
