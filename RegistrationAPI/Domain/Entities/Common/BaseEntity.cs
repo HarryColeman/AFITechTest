0c2485ccb21e5f3f175c4e44ac82f90b405ce2f5
hcoleman.r@gmail.com <hcoleman.r@gmail.com>
2024-02-20 18:14:35 +0000

    Added Domain and PolicyHolder entity

---
+++ RegistrationAPI/Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace RegistrationAPI.Domain.Entities.Common;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ RegistrationAPI/Domain/Entities/PolicyHolder.cs
@@ -0,0 +1,13 @@
+﻿using RegistrationAPI.Domain.Entities.Common;
+
+namespace RegistrationAPI.Domain.Entities;
+public class PolicyHolder : BaseEntity
+{
+    public int OnlineCustomerId => Id;
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string ReferenceNumber { get; set; }
+    public string? Email { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+
+}
